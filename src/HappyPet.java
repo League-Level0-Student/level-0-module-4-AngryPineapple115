6f0cd80b6b2cc86c4435b4f86a9a884ff81c50e5
league <league@wts-im-17.attlocal.net>
2018-05-29 17:01:51 -0700

    HappyPet ip

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -1,29 +1,56 @@
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
-	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+
+	// 2. Add the following variable to the next line: static int happinessLevel =
+	// 0;
 	// this will be used to store the happiness of your pet
-	
+
+	static int happinesslevel = 0;
+	static String pet;
+
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in
+		// a variable
+
+		pet = JOptionPane.showInputDialog("What kind of pet would you like to buy?");
 
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+		int task = 0;
+		for (int i = 0; i < 5; i++) {
 
+			// 3. Use showOptionDialog to ask the user what they want to do to make their
+			// pet happy
+			// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+			// Make sure to customize the title and question too.
+			task = JOptionPane.showOptionDialog(null, "Pick one:", "Make your " + pet + " happier!", 0,
+					JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Feed", "Play", "Groom" }, null);
+			// 0>1>2
 			// 5. Use user input to call the appropriate method created in step 4.
-
+			if (task == 2) {
+				groom();
+			}
 			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
+			// user that he loves his pet and use break; to exit for loop.
+		}
+		JOptionPane.showMessageDialog(null, "You love your pet!");
+		// 4. Create methods to handle each of your user selections.
+		// Each method should create a pop-up with the pet's response (eg. cat might
+		// purr when pet),
+		// and INCREMENT the pet's happiness Level.
 
 	}
 
-	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
+	static void groom() {
+		JOptionPane.showMessageDialog(null, "Your " + pet + " Just won the prize for 'Best Groomed " + pet + "!'");
+		happinesslevel++;
+	}
+	static void play() {
+		JOptionPane.showMessageDialog(null, "Your " + pet );
+		happinesslevel++;
+	}
+	static void feed() {
+		JOptionPane.showMessageDialog(null, "Your " + pet + " Just won the prize for 'Best Groomed " + pet + "!'");
+		happinesslevel++;
+	}
 }
--- src/OwnAdventure.java
+++ src/OwnAdventure.java
@@ -4,18 +4,14 @@ public class OwnAdventure {
 
 	public static void main(String[] args) {
 		
-		String q1 = JOptionPane.showInputDialog("The "
-				+ "warrior walks through the forest as he "
-				+ "searches for a quest to complete. He "
-				+ "stops, and looks up to find two tall doors "
-				+ "sitting in front of him. Should he open"
-				+ " the left one or the right one?");
+		String q1 = JOptionPane.showInputDialog("The warrior walks through the forest as he searches for a quest "
+				+ "to complete. He stops, and looks up to find two tall doors sitting in front of him. Should he open the left one or the right one?");
 		
 		if (q1.equalsIgnoreCase("Left")) {
-			String q2 = JOptionPane.showInputDialog("He pushes open the door and looks around.");
+			String q2 = JOptionPane.showInputDialog("He pushes the door open and looks around.");
 		}
-		else {
-			
+		else if (q1.equalsIgnoreCase("Right")){
+			String q3 = JOptionPane.showMessageDialog(null, "He looks around the room. He spots a hole leading donward,"); 
 		}
 		}
 }
