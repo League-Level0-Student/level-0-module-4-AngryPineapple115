3c5d1b995dd97111fa6f2be05d222e64aabe4482
league <league@wts8.attlocal.net>
2018-06-12 17:00:31 -0700

    OwnAdventure ip

--- src/OwnAdventure.java
+++ src/OwnAdventure.java
@@ -1,17 +1,38 @@
+
+//911 = 2, Stay there = 1, Help = 0
+//Empty = 2, Lava = 1, Water = 0
+import java.awt.Choice;
+
 import javax.swing.JOptionPane;
 
 public class OwnAdventure {
 
 	public static void main(String[] args) {
-		
-		String q1 = JOptionPane.showInputDialog("The warrior walks through the forest as he searches for a quest "
-				+ "to complete. He stops, and looks up to find two tall doors sitting in front of him. Should he open the left one or the right one?");
-		
-		if (q1.equalsIgnoreCase("Left")) {
-			String q2 = JOptionPane.showInputDialog("He pushes the door open and looks around.");
+
+		int choice = 0;
+		choice = JOptionPane.showOptionDialog(null,
+				"It was a bright, sunny Monday, and Sticky the Stickman was sitting in a hole in his lawn. He heard someone cry for help. What should he do?",
+				"Choose sticky's path!", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Help the person!", "Stay where you are; it's probably not THAT important." }, null);
+		System.out.println(choice);
+		int help = 0;
+		if (choice == 0) {
+			
+
+			help = JOptionPane.showOptionDialog(null,
+					"Sticky runs over to the man frantically waving his hand in the air.\n Help! My kids treehouse is on FIRE! \n Um...I don't really know how to help, but "
+							+ "I have a lot of buckets down in my basement, and I could fill them up with something.",
+					"Choose Sticky's path!", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Fill the buckets with water.", "Fill up the buckets with lava.",
+							"Leave all the buckets empty." },
+					null);
+			System.out.println(help);
+
+		} else if (choice == 1) {
+			JOptionPane.showMessageDialog(null, "Sticky stays in the hole and starts to fall asleep, because he didn't feel like helping anyone on a Monday. \n THE END!!");
 		}
-		else if (q1.equalsIgnoreCase("Right")){
-			String q3 = JOptionPane.showMessageDialog(null, "He looks around the room. He spots a hole leading donward,"); 
+		if (help == 1) {
+			JOptionPane.showMessageDialog(null, "Sticky returns from his basement carrying many buckets, looking VERY tired. ");
 		}
-		}
+	}
 }
